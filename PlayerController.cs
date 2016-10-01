e3393c91dff8c3d992e54f57fa585be42e133627
kNelakonda <kavi.nelakonda@gmail.com>
2016-10-01 00:17:37 -0700

    scripting supplements!

---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float lowestAllowed = -20; 
+	private Vector3 startLocation;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		startLocation = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (transform.position.y <= lowestAllowed) {
+			transform.position = startLocation;
+		}
+	}
+
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
