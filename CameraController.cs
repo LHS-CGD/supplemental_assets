f1daffcbe80c2cc04bf0ff18a8e330ff060517a2
kNelakonda <kavi.nelakonda@gmail.com>
2016-10-31 23:21:27 -0700

    new updates for the next meeting!

--- CameraController.cs
+++ CameraController.cs
@@ -4,15 +4,27 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject myo = null;
+	private ThalmicMyo thalmicMyo;
+	private Transform playerTransform;
 
 	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
+		playerTransform = player.transform;
 		offset = transform.position - player.transform.position;
+		thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
 	}
-	
+
+	void Update() {
+		this.transform.rotation = new Quaternion(thalmicMyo.transform.rotation.x, thalmicMyo.transform.rotation.y,
+			thalmicMyo.transform.rotation.z, thalmicMyo.transform.rotation.w);
+	}
+
 	void LateUpdate () {
+		
 		transform.position = player.transform.position + offset;
 	}
 }
