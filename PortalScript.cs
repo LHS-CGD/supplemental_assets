6ea6a4acda6115f3ad597aa8d063970d2a34503a
kNelakonda <kavi.nelakonda@gmail.com>
2016-11-08 10:30:41 -0800

    new script

---
+++ PortalScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalScript : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	public GameObject portal2;
+	private Vector3 newSpot;
+	// Use this for initialization
+	void Start (){
+		newSpot = portal2.transform.position;
+
+	}
+
+	void OnTriggerEnter(Collider portal)
+	{
+		player.transform.position = newSpot;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
